45a0674cdbc906ef14e0eab5f487edc9848718f6
erdemdibek <erdemdibek9393@gmail.com>
2026-07-02 07:11:39 +0300

    Create armor_smithing.js

---
+++ recipes/armor_smithing.js
@@ -0,0 +1,4 @@
+export const armorSmithingRecipes = [
+    { id: "lvl1_elite_gear", name: "Lv1 - Elite Boots / Gloves (+13000 XP)", levelRequired: 1, xpGiven: 13000, isChain: false, materials: { stag: 4, boar: 0, zebra: 0, copper: 4, iron: 0, baseItem: 1 } },
+    { id: "lvl10_imperial_gear", name: "Lv10 - Imperial Boots / Gloves (+26000 XP)", levelRequired: 10, xpGiven: 26000, isChain: false, materials: { stag: 4, boar: 2, zebra: 1, copper: 4, iron: 2, baseItem: 1 } }
+];
