9a6fc16e24300357fb6b0912a70e04c75dfa35e8
erdemdibek <erdemdibek9393@gmail.com>
2026-07-02 07:30:00 +0300

    Update stonemasonry.js

--- recipes/stonemasonry.js
+++ recipes/stonemasonry.js
@@ -1,1 +1,26 @@
-export const stonemasonryRecipes = [];
+export const stonemasonryRecipes = [
+    { 
+        id: "stone_block", 
+        name: "Lv1 - Stone Block (+260 XP)", 
+        levelRequired: 1, 
+        xpGiven: 260, 
+        isChain: false, 
+        materials: { roughStone: 5 } 
+    },
+    { 
+        id: "marble_block", 
+        name: "Lv20 - Marble Block (+1560 XP)", 
+        levelRequired: 20, 
+        xpGiven: 1560, 
+        isChain: false, 
+        materials: { marble: 5 } 
+    },
+    { 
+        id: "granite_block", 
+        name: "Lv30 - Granite Block (+7800 XP)", 
+        levelRequired: 30, 
+        xpGiven: 7800, 
+        isChain: false, 
+        materials: { granite: 5 } 
+    }
+];
