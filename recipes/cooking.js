746df17d97cea909d09c22cf1a91ec361324b878
erdemdibek <erdemdibek9393@gmail.com>
2026-07-02 07:08:38 +0300

    Create recipes/cooking.js

---
+++ recipes/cooking.js
@@ -0,0 +1,8 @@
+export const cookingRecipes = [
+    { id: "butter", name: "Lv1 - Butter (+10 XP)", levelRequired: 1, xpGiven: 10, isChain: false },
+    { id: "roasted_corn", name: "Lv1 - Roasted Corn (+52 XP)", levelRequired: 1, xpGiven: 52, isChain: false },
+    { id: "roasted_potato", name: "Lv1 - Roasted Potato (+52 XP)", levelRequired: 1, xpGiven: 52, isChain: false },
+    { id: "carrot_soup", name: "Lv4 - Carrot Soup (+124 XP)", levelRequired: 4, xpGiven: 124, isChain: false },
+    { id: "omlette", name: "Lv4 - Omlette (+124 XP)", levelRequired: 4, xpGiven: 124, isChain: false },
+    { id: "tomato_soup", name: "Lv4 - Tomato Soup (+124 XP)", levelRequired: 4, xpGiven: 124, isChain: false }
+];
