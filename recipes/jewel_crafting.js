53985a578392e6df0bf24aa464d707675c95c6c6
erdemdibek <erdemdibek9393@gmail.com>
2026-07-07 13:58:32 +0300

    Create jewel_crafting.js

---
+++ recipes/jewel_crafting.js
@@ -0,0 +1,109 @@
+export const jewelCraftingRecipes = [
+    // Seviye 1 - Değerli Taş Reçeteleri
+    { 
+        id: "rare_amber", 
+        name: "Rare Amber", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: true, // İleride üst seviye takı yapımında kullanılabilir
+        materials: { 
+            crude_amber: 3 
+        } 
+    },
+    { 
+        id: "rare_amethyst", 
+        name: "Rare Amethyst", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: true, 
+        materials: { 
+            crude_amethyst: 3 
+        } 
+    },
+    { 
+        id: "rare_emerald", 
+        name: "Rare Emerald", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: true, 
+        materials: { 
+            crude_emerald: 3 
+        } 
+    },
+    { 
+        id: "rare_obsidian", 
+        name: "Rare Obsidian", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: true, 
+        materials: { 
+            stoneblock: 1, 
+            sphere_of_fire: 1, 
+            sphere_of_water: 1 
+        } 
+    },
+    { 
+        id: "rare_ruby", 
+        name: "Rare Ruby", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: true, 
+        materials: { 
+            crude_ruby: 3 
+        } 
+    },
+    { 
+        id: "rare_topaz", 
+        name: "Rare Topaz", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: true, 
+        materials: { 
+            crude_topaz: 3 
+        } 
+    },
+    { 
+        id: "rare_sapphire", 
+        name: "Rare Sapphire", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: true, 
+        materials: { 
+            crude_sapphire: 3 
+        } 
+    },
+
+    // Seviye 1 - Temel Takı Dönüşüm Reçeteleri
+    { 
+        id: "basic_earring", 
+        name: "Basic Earring", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: false, 
+        materials: { 
+            golden_earring_plus8: 1 
+        } 
+    },
+    { 
+        id: "basic_ring", 
+        name: "Basic Ring", 
+        levelRequired: 1, 
+        xpGiven: 520, 
+        isIntermediate: false, 
+        materials: { 
+            golden_ring_plus8: 1 
+        } 
+    },
+
+    // Seviye 1 - Kolye Reçetesi
+    { 
+        id: "basic_necklace_chain", 
+        name: "Basic Necklace", 
+        levelRequired: 1, 
+        xpGiven: 1040, 
+        isIntermediate: false, 
+        materials: { 
+            golden_necklace_plus9: 1 
+        } 
+    }
+];
